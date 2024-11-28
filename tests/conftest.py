cd50e9f838b7878937afae35709d79fa9e676284
mohit <mtejsinghani8660@gmail.com>
2024-11-28 15:14:48 +0530

    corrected the conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ from app.config import settings
 from app.database import get_db, Base
 from app.oauth2 import create_access_token
 from alembic import command
+from app import models
 
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test'
