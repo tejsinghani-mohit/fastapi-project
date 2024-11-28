8046c9a2e3da06dd8c7abb20f6670173a6fa5c95
mohit <mtejsinghani8660@gmail.com>
2024-11-28 14:58:58 +0530

    updated conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from app.main import app
 from jose import jwt
 from app.config import settings
 from app.database import get_db, Base
+from app.oauth2 import create_access_token
 from alembic import command
 
 
@@ -46,3 +47,16 @@ def test_user(client):
     new_user['password'] = user_data['password']
     # print(res.json())
     return new_user    
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
